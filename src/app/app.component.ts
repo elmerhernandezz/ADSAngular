cffb5ee88540c31c14f3326c1ffb5f2f3ea1fff9
elmerhernandezz <elmeralbertoperez@gmail.com>
2024-05-17 11:36:52 -0600

    Agregando endpoints para conectarse a el API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { EstudiantesService } from './services/estudiantes.service';
+import { CarrerasService } from './services/carreras.service';
+import { GruposService } from './services/grupos.service';
+import { MateriasService } from './services/materias.service';
+import { ProfesoresService } from './services/profesores.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'GestionAcademica';
+  // Haciendo inyeccion de dependencia
+  private readonly estudiantesServices = inject(EstudiantesService);
+  // Creando observable
+  estudiantes$ = this.estudiantesServices.obtenerEstudiantes();
+  // Haciendo inyeccion de dependencia
+  private readonly carrerasServices = inject(CarrerasService);
+  // Creando observable
+  carreras$ = this.carrerasServices.obtenerCarreras();
+  // Haciendo inyeccion de dependencia
+  private readonly gruposServices = inject(GruposService);
+  // Creando observable
+  grupos$ = this.gruposServices.obtenerGrupos();
+  // Haciendo inyeccion de dependencia
+  private readonly materiasServices = inject(MateriasService);
+  // Creando observable
+  materias$ = this.materiasServices.obtenerMaterias();
+  // Haciendo inyeccion de dependencia
+  private readonly profesoresServices = inject(ProfesoresService);
+  // Creando observable
+  profesores$ = this.profesoresServices.obtenerProfesores();
 }
---
+++ src/app/utils/endpoints.ts
@@ -0,0 +1,65 @@
+import { environment } from "../../environments/environment.development";
+
+export const endpoints = {
+
+    // *** Estudiantes ***
+    agregarEstudiante:
+environment.serverURL.concat('api/estudiantes/agregarEstudiante'),
+    actualizarEstudiante:
+environment.serverURL.concat('api/estudiantes/actualizarEstudiante/:idEstudiante'),
+    eliminarEstudiante:
+environment.serverURL.concat('api/estudiantes/eliminarEstudiante/:idEstudiante'),
+    obtenerEstudiantePorID:
+environment.serverURL.concat('api/estudiantes/obtenerEstudiantePorID/:idEstudiante'),
+    obtenerEstudiantes:
+environment.serverURL.concat('api/estudiantes/obtenerEstudiantes'),
+    // ***********************
+    // *** Carreras ***
+    agregarCarrera:
+environment.serverURL.concat('api/carreras/agregarCarrera'),
+    actualizarCarrera:
+environment.serverURL.concat('api/carreras/actualizarCarrera/:idCarrera'),
+    eliminarCarrera:
+environment.serverURL.concat('api/carreras/eliminarCarrera/:idCarrera'),
+    obtenerCarreraPorID:
+environment.serverURL.concat('api/carreras/obtenerCarreraPorID/:idCarrera'),
+    obtenerCarreras:
+environment.serverURL.concat('api/carreras/obtenerCarreras'),
+    // ***********************
+    // *** Grupos ***
+    agregarGrupo:
+environment.serverURL.concat('api/grupos/agregarGrupo'),
+    actualizarGrupo:
+environment.serverURL.concat('api/grupos/actualizarGrupo/:idGrupo'),
+    eliminarGrupo:
+environment.serverURL.concat('api/grupos/eliminarGrupo/:idGrupo'),
+    obtenerGrupoPorID:
+environment.serverURL.concat('api/grupos/obtenerGrupoPorID/:idGrupo'),
+    obtenerGrupos:
+environment.serverURL.concat('api/grupos/obtenerGrupos'),
+    // ***********************
+    // *** Materias ***
+    agregarMateria:
+environment.serverURL.concat('api/materias/agregarMateria'),
+    actualizarMateria:
+environment.serverURL.concat('api/materias/actualizarMateria/:idMateria'),
+    eliminarMateria:
+environment.serverURL.concat('api/materias/eliminarMateria/:idMateria'),
+    obtenerMateriaPorID:
+environment.serverURL.concat('api/materias/obtenerMateriaPorID/:idMateria'),
+    obtenerMaterias:
+environment.serverURL.concat('api/materias/obtenerMaterias'),
+    // ***********************
+    // *** Profesores ***
+    agregarProfesor:
+environment.serverURL.concat('api/profesores/agregarProfesor'),
+    actualizarProfesor:
+environment.serverURL.concat('api/profesores/actualizarProfesor/:idProfesor'),
+    eliminarProfesor:
+environment.serverURL.concat('api/profesores/eliminarProfesor/:idProfesor'),
+    obtenerProfesorPorID:
+environment.serverURL.concat('api/profesores/obtenerProfesorPorID/:idProfesor'),
+    obtenerProfesores:
+environment.serverURL.concat('api/profesores/obtenerProfesores')
+    // ***********************
+};
