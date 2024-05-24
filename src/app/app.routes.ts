f3b7466d1834f01600b0987d5b2496711d58aae5
elmerhernandezz <elmeralbertoperez@gmail.com>
2024-05-24 15:33:34 -0600

    Se agrego los routes correctamente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,18 +6,10 @@ import { MateriasComponent } from './materias/materias.component';
 import { ProfesoresComponent } from './profesores/profesores.component';
 
 export const routes: Routes = [
-    {path: '', component: ProfesoresComponent, pathMatch: 'full'}, // Ruta por defecto
-    {path: '**', redirectTo: '', pathMatch: 'full'}, // Rutas no existentes
-
-    {path: '', component: MateriasComponent, pathMatch: 'full'}, // Ruta por defecto
-    {path: '**', redirectTo: '', pathMatch: 'full'}, // Rutas no existentes
-
-    {path: '', component: GruposComponent, pathMatch: 'full'}, // Ruta por defecto
-    {path: '**', redirectTo: '', pathMatch: 'full'}, // Rutas no existentes
-
-    {path: '', component: EstudiantesComponent, pathMatch: 'full'}, // Ruta por defecto
-    {path: '**', redirectTo: '', pathMatch: 'full'}, // Rutas no existentes
-
-    {path: '', component: CarrerasComponent, pathMatch: 'full'}, // Ruta por defecto
-    {path: '**', redirectTo: '', pathMatch: 'full'} // Rutas no existentes
+    { path: '', component: EstudiantesComponent, pathMatch: 'full' }, // Ruta por defecto
+    { path: 'materias', component: MateriasComponent },
+    { path: 'grupos', component: GruposComponent },
+    { path: 'carreras', component: CarrerasComponent },
+    { path: 'profesores', component: ProfesoresComponent },
+    { path: '**', redirectTo: '', pathMatch: 'full' } // Rutas no existentes
 ];
